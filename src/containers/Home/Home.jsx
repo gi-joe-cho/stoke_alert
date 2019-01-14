355ecf4e0f01a09428cbc866a1a0cec914396239
Joseph Cho <josephcho@Josephs-MacBook-Pro-2.local>
2019-01-14 13:24:42 -0800

    created landing page. #1

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,87 +1,71 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
-// import MapContainer from '../../components/Home/MapContainer';
-// import SurferPosts from '../../components/Home/SurferPosts';
+import { Segment, Button, Dropdown, Header, Icon } from 'semantic-ui-react';
+import TabBar from '../../components/TabBar/TabBar';
+
+const options = [
+    { key: 1, text: 'One', value: 1 },
+    { key: 2, text: 'Two', value: 2 },
+    { key: 3, text: 'About', value: 3 },
+]
+
 class Home extends Component {
-    state = {
-        coordinates: {
-            latitude: null,
-            longitude: null,
-        },
-        surferPosts: [1,2,3,4,5,6,7,8]
-    }
-
-    // componentDidMount() {
-    //     this.requestPosts();
-    // }
-
-    // requestPosts = async () => {
-    //     const posts = await getPosts();
-    //     this.setState({ surferPosts: posts });
-    // }
+    state = {}
 
     render() {
-        const { coordinates, surferPosts } = this.state;
         return (
-            <div>
-                {/* <MapContainer coordinates={coordinates} /> */}
-                {/* <SurferPosts surferPosts={ surferPosts } /> */}
-                
-                
-                    <div className="div-thang">
-                        
-                        <img class="ocean-pic" src="https://images.pexels.com/photos/7321/sea-water-ocean-horizon.jpg?auto=compress&cs=tinysrgb&h=750&w=1260"/>
+            <div className="wrapper-container">
+                <Segment className="div-thang" raised>
+                    <TabBar name="Map" />
+                    <Segment className="home-ocean-pic">
+                        <Segment className="home-row" stacked>
 
-                        {/* <div className="text-div">
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                            <p>S t 0 K 3 - A  l  e  R  t</p>
-                    </div> */}
-                </div>
-               
+                            <Segment className="map-container" placeholder raised>
+                                <div className="map">
+                                    <p>MAP WILL GO HERE</p>
+                                    <span>S t 0 K 3 - A  l  e  R  t</span>
+                                    <span>S t 0 K 3 - A  l  e  R  t</span>
+                                    <span>S t 0 K 3 - A  l  e  R  t</span>
+                                    <span>S t 0 K 3 - A  l  e  R  t</span>
+                                </div>
+                            </Segment>
 
+                            <Segment className="map-list-container" placeholder raised>
+                                <div>
+                                    <p>MAP LIST WILL GO HERE</p>
+                                    <p>S t 0 K 3 - A  l  e  R  t</p>    
+                                </div>
+                            </Segment>
+                        </Segment>
+                    </Segment>    
+
+                    <div className="start-menu">
+                        <Dropdown text="Start" upward icon="chevron up" floating labeled button selection options={options} id="right-arrow" className="icon" > 
+                            <Dropdown.Menu>
+                                <Dropdown.Header fluid icon='hand peace outline' content='Stoke_Alert_Menu_' />
+                                <Dropdown.Item>Important</Dropdown.Item>
+                                <Dropdown.Item>Announcement</Dropdown.Item>
+                                <Dropdown.Item icon='facebook official' content='Facebook' subheader='Stoke_Alert_ Facebook page' />
+                                <Dropdown.Item icon='facebook official' content='Facebook' subheader='Stoke_Alert_ Facebook page' />
+                                <Dropdown.Item icon='question' text='About' />
+                                <Header icon='facebook official' content='Facebook' subheader='Stoke_Alert_ Facebook page'/>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Button className="start-menu-icon" icon>
+                            <Icon  name='facebook official' size='large' />
+                        </Button>
+                        <Button className="start-menu-icon" icon>
+                            <Icon name='instagram' size='large'/>
+                        </Button>
+                        <Button className="start-menu-icon" icon>
+                            <Icon name='text width' size='large' />
+                        </Button>
+                    </div>     
+
+                </Segment>   
             </div>
         )
     }
 }
-
 
 export default Home;
