acfa462facd196f9d4dfd2d11f446cde99ad5af0
Joseph Cho <josephcho@Josephs-MacBook-Pro-2.local>
2019-01-14 13:36:33 -0800

    created reusable TabBar component. #1

---
+++ src/components/TabBar/TabBar.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { props } from 'bluebird';
+
+class TabBar extends Component {
+  render() {
+    return(
+      <div className="nav-bar">
+        <div className="title-bar">
+          <ul>
+            <li> <span>S t o k e_A l e r t_{props.name}</span></li>
+            <button className="btn-title-bar">x</button>
+          </ul>
+        </div>
+        <div className="menu-bar" >
+          <ul>
+            <li><a className="back-tab" href="news.asp">Back</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">Profile</a></li>
+          </ul>
+        </div>
+      </div>
+    ); 
+  }
+}
+
+export default TabBar;
+
+
