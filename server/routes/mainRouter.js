6dbab2ba76bf9543058eef376cf368131ee91f94
Ryan Shin <changyeolshin@gmail.com>
2019-01-20 22:08:04 -0800

    Imported the postsRouter to the mainRouter. #8

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const usersRouter = require('./usersRouter');
+const postsRouter = require('./postsRouter');
 
 const mainRouter = knex => {
   const router = new Router();
   return router
-    .use('/users', usersRouter(knex));
+    .use('/users', usersRouter(knex))
+    .use('/posts', postsRouter(knex));
 };
 
 module.exports = mainRouter;
