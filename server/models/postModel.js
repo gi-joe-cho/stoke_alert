ac444f520c953edce4d5d2d4c79355bd4355d2ec
Ryan Shin <changyeolshin@gmail.com>
2019-01-20 23:49:29 -0800

    Provided correct indentations for the PostModel. #8

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,71 +1,71 @@
 class PostModel {
   constructor(post) {
-      this.id = post.id;
-      this.user_id = post.user_id;
-      this.user_rating = post.user_rating;
-      this.up_votes = post.up_votes;
-      this.down_votes = post.down_votes;
-      this.image_location_url = post.image_location_url;
-      this.post_content = post.image_location_url;
-      this.lat = post.lat;
-      this.lng = post.lng;
-      this.city = post.city;
-      this.state = post.state;
-      this.zipcode = post.zipcode;
-      this.created_at = post.created_at;
-      this.updated_at = post.updated_at;
-      this.first_name = post.first_name;
-      this.last_name = post.last_name;
-      this.username = post.username;
-      this.email = post.email;
+    this.id = post.id;
+    this.user_id = post.user_id;
+    this.user_rating = post.user_rating;
+    this.up_votes = post.up_votes;
+    this.down_votes = post.down_votes;
+    this.image_location_url = post.image_location_url;
+    this.post_content = post.image_location_url
+    this.lat = post.lat;
+    this.lng = post.lng;
+    this.city = post.city;
+    this.state = post.state;
+    this.zipcode = post.zipcode;
+    this.created_at = post.created_at;
+    this.updated_at = post.updated_at;
+    this.first_name = post.first_name;
+    this.last_name = post.last_name;
+    this.username = post.username;
+    this.email = post.email;
   }
 
   set setId(id) {
-      this.id = id;
+    this.id = id;
   }
 
   setUserId(user_id) {
-      this.user_id = user_id;
+    this.user_id = user_id;
   }
 
   setUpVotes(up_votes) {
-      this.up_votes = up_votes;
+    this.up_votes = up_votes;
   }
 
   setDownVotes(down_votes) {
-      this.down_votes = down_votes;
+    this.down_votes = down_votes;
   }
 
   setImageLocationUrl(image_location_url) {
-      this.image_location_url = image_location_url;
+    this.image_location_url = image_location_url;
   }
 
   setPostContent(post_content) {
-      this.post_content = post_content;
+    this.post_content = post_content;
   }
 
   setLat(lat) {
-      this.lat = lat;
+    this.lat = lat;
   }
 
   setLng(lng) {
-      this.lng = lng;
+    this.lng = lng;
   }
 
   setCity(city) {
-      this.city = city;
+    this.city = city;
   }
 
   setState(state) {
-      this.state = state;
+    this.state = state;
   }
 
   setZipcode(zipcode) {
-      this.zipcode = zipcode;
+    this.zipcode = zipcode;
   }
 
   setCreatedAt(created_at) {
-      this.created_at = created_at;
+    this.created_at = created_at;
   }
 
   setUpdatedAt(updated_at) {
@@ -73,11 +73,11 @@ class PostModel {
   }
 
   setFirstName(first_name) {
-      this.first_name = first_name;
+    this.first_name = first_name;
   }
 
   setLastName(last_name) {
-      this.last_name = last_name;
+    this.last_name = last_name;
   }
 
   setUsername(username) {
@@ -170,22 +170,22 @@ class PostModel {
   }
 
   get post() {
-      return {
-          id: this.id,
-          user_rating: this.user_rating,
-          up_votes: this.up_votes,
-          down_votes: this.down_votes,
-          image_location_url: this.image_location_url,
-          post_content: this.post_content,
-          lat: this.lat,
-          lng: this.lng,
-          city: this.city,
-          state: this.state,
-          zipcode: this.zipcode,
-          created_at: this.created_at,
-          updated_at: this.updated_at,
-          user: this.user(),
-      }
+    return {
+      id: this.id,
+      user_rating: this.user_rating,
+      up_votes: this.up_votes,
+      down_votes: this.down_votes,
+      image_location_url: this.image_location_url,
+      post_content: this.post_content,
+      lat: this.lat,
+      lng: this.lng,
+      city: this.city,
+      state: this.state,
+      zipcode: this.zipcode,
+      created_at: this.created_at,
+      updated_at: this.updated_at,
+      user: this.user(),
+    }
   }
 }
 
