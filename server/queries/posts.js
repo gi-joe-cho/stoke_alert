de6a6bb090c8f7a30fc80ced433afaf185ec9c49
Ryan Shin <changyeolshin@gmail.com>
2019-01-22 09:45:26 -0800

    Fixed the findPostsWithinRadius query method. #8

--- server/queries/posts.js
+++ server/queries/posts.js
@@ -1,11 +1,29 @@
 const findPostsWithinRadius = (posts, lat, lng) =>
   posts
     .clone()
+    .select([
+      'posts.id',
+      'posts.user_id',
+      'posts.user_rating',
+      'posts.up_votes',
+      'posts.post_content',
+      'posts.lat',
+      'posts.lng',
+      'posts.city',
+      'posts.state',
+      'posts.zipcode',
+      'posts.created_at',
+      'posts.updated_at',
+      'users.username',
+      'users.first_name',
+      'users.last_name',
+      'users.email',
+    ])
     .rightJoin('users', 'posts.user_id', 'users.id')
-    .where("lat", "<=", lat)
-    .andWhere("lng", "<=", lng)
-    .orderBy("updated_at", "desc")
+    .where('lat', '<=', lat)
+    .andWhere('lng', '<=', lng)
     .limit(10)
+    .orderBy('posts.updated_at', 'desc')
     .then(posts => posts);
 
 module.exports = {
