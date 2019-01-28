711ffa4dbde69785703bcb48407423f44f66a61c
Joseph Cho <josephcho@Josephs-MacBook-Pro-2.local>
2019-01-27 19:05:27 -0800

    ryanchangyeolshin - Fixed spacing. #10

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import { Icon, Button, Popup } from 'semantic-ui-react';
 
 const TabBar = ({ closeModal}) => (
-      <div className="nav-bar">
-        <div className="title-bar">
-          <ul>
-            <li> <span>S t o k e_A l e r t_</span></li>
-            <Button onClick={closeModal} id="close-window">
-              <Popup trigger={<Icon name="close" />} content='Close Window' />
-            </Button>
-          </ul>
-        </div>
-        <div className="menu-bar" >
-          <ul>
-            <li><a className="back-tab" href="news.asp">Back</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">Profile</a></li>
-          </ul>
-        </div>
-      </div>
-)
+  <div className="nav-bar">
+    <div className="title-bar">
+      <ul>
+        <li> <span>S t o k e_A l e r t_</span></li>
+        <Button onClick={closeModal} id="close-window">
+          <Popup trigger={<Icon name="close" />} content='Close Window' />
+        </Button>
+      </ul>
+    </div>
+    <div className="menu-bar" >
+      <ul>
+        <li><a className="back-tab" href="news.asp">Back</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">Profile</a></li>
+      </ul>
+    </div>
+  </div>
+);
+
 export default TabBar;
 
 
