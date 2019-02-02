ecda2b763357ab0072add938193adf1657055d22
Ryan Shin <changyeolshin@gmail.com>
2019-02-01 22:31:41 -0800

    Ensure the postsRouter tests pass with the new fakeData.js helper functions. #8

--- tests/server/routes/postsRouter.test.js
+++ tests/server/routes/postsRouter.test.js
@@ -4,49 +4,20 @@ const bcrypt = require('bcrypt');
 const faker = require('faker');
 const users = require('../knex')('users');
 const posts = require('../knex')('posts');
+const { createFakeUser, createFakePost } = require('../../fakeData');
 
 describe('testing the postsRouter', async () => {
-  const fakePassword = 'andiechoie1991';
   let fakeUser;
   const fakePosts = [];
 
   beforeEach(async (done) => {
-    const saltRounds = 10;
-    const hashedPassword = bcrypt.hashSync(fakePassword, saltRounds);
-    fakeUser = {
-      id: uuid(),
-      first_name: 'Andie',
-      last_name: 'Choie',
-      username: 'achoie91',
-      email: 'andie_choie@gmail.com',
-      password: hashedPassword,
-      birth_date: new Date('02-01-1991'),
-      city: 'Los Angeles',
-      state: 'CA',
-      zipcode: '90007',
-      annotation: 'I am an architectural engineer who enjoys powerlifting.',
-    };
+    fakeUser = createFakeUser();
     await users
       .clone()
       .insert(fakeUser);
 
-    const user_ratings = ["Awful", "Bad", "Average", "Good", "Great", "Gnarly"];
-    const randomUserRatingsIndex = Math.floor(Math.random() * user_ratings.length);
     for (let i = 0; i < 10; i++) {
-      const fakePost = {
-        id: uuid(),
-        user_id: fakeUser.id,
-        user_rating: user_ratings[randomUserRatingsIndex],
-        up_votes: faker.random.number(),
-        down_votes: faker.random.number(),
-        image_location_url: faker.image.imageUrl(),
-        post_content: faker.lorem.sentence(),
-        lat: faker.random.number() * 1.000,
-        lng: faker.random.number() * 1.000,
-        city: faker.address.city(),
-        state: faker.address.state(),
-        zipcode: faker.address.zipCode(),
-      };
+      const fakePost = createFakePost(fakeUser);
       fakePosts.push(fakePost);
 
       await posts
