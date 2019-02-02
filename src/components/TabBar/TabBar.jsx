fabf24989ba7dd3c6d4dd3c59639719825dd9592
Joseph Cho <josephcho@Josephs-MBP-2.lan>
2019-02-02 00:02:20 -0800

    add dynamic flexibility for header display on all uses of TabBar. #10

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Icon, Button, Popup } from 'semantic-ui-react';
 
-const TabBar = ({ closeModal}) => (
+const TabBar = ({ closeModal, tabMessage}) => (
   <div className="nav-bar">
     <div className="title-bar">
       <ul>
-        <li> <span>S t o k e_A l e r t_</span></li>
+        <li> <span>S t o k e_A l e r t_{tabMessage}</span></li>
         <Button onClick={closeModal} id="close-window">
           <Popup trigger={<Icon name="close" />} content='Close Window' />
         </Button>
