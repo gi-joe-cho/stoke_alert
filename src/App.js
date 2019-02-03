2967e2b32480187268a844d5ae7916b6983e5cd2
Joseph Cho <josephcho@Josephs-MacBook-Pro-2.local>
2019-02-03 15:21:40 -0800

    removed state variables and refactored dynamic header display functionality in inidividual containers. #10

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import Navbar from './containers/Navbar/Navbar';
 import Home from './containers/Home/Home';
 
 class App extends Component {
-  state = {
-    textBool: true
-  }
   render() {
-  const { textBool } = this.state
     return (
       <div>
-        <Navbar textBool={textBool} />
-        <Home textBool={textBool}/>
+        <Navbar />
+        <Home />
       </div>
     );
   }
