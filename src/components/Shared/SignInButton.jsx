a3eefb00c102bf1c06c3c7f10e2caddcdfeb1ae9
Joseph Cho <josephcho@Josephs-MacBook-Pro-2.local>
2019-02-03 18:57:02 -0800

    removed spaces. #10

--- src/components/Shared/SignInButton.jsx
+++ src/components/Shared/SignInButton.jsx
@@ -3,7 +3,7 @@ import { Icon, Button, Popup } from 'semantic-ui-react';
 
 const SignInButton = ({ closeConfig }) => (
   
-  <Button onClick={ closeConfig } className="windows-btn" id="login">
+  <Button onClick={closeConfig} className="windows-btn" id="login">
     <span className='window-btn-span-cancel'></span>
     <Popup trigger={<Icon name="sign-in" />} content='Sign-in to your account' />
   </Button>
--- src/components/Shared/SignUpButton.jsx
+++ src/components/Shared/SignUpButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Icon, Button, Popup } from 'semantic-ui-react';
 
  const SignUpButton = ({ closeConfig }) => (
-  <Button onClick={ closeConfig } id="login">
+  <Button onClick={closeConfig} id="login">
     <Popup trigger={<Icon name="user circle" />} content='Dont have an account? Sign up for an account' />
   </Button>
 )
