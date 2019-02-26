dc49b1dfa67601fc38f32ebfb9335d87de728547
Ryan Shin <changyeolshin@gmail.com>
2019-02-25 19:39:40 -0800

    Created a middleware function called verifySessionTokenForAccess. #15

--- server/utils/validations.js
+++ server/utils/validations.js
@@ -39,8 +39,25 @@ const validateSessionTokenExpiration = async ({ body: { token }}, res, next) => 
   next();
 };
 
+const verifySessionTokenForAccess = async ({ body: { token }}, res, next) => {
+  if (!token) {
+    return res.status(401).jsonp({ message: 'Session token is unavailable!' });
+  }
+  const expired = jwt.verify(token, process.env.JWT_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return true;
+    }
+  });
+  if (expired) {
+    return res.status(401).jsonp({ message: "Token has expired! Try signing in again!" });
+  }
+
+  next();
+};
+
 module.exports = {
   checkForDuplicateNameAndEmail,
   checkSessionTokenExists,
-  validateSessionTokenExpiration
+  validateSessionTokenExpiration,
+  verifySessionTokenForAccess,
 };
