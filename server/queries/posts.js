9bc4c4d666005a3df82080732d3a6f0847dbedba
Ryan Shin <changyeolshin@gmail.com>
2019-04-03 16:33:45 -0700

    Updated the createSurferPost insert query to clone and the /posts/:user_id/create route to handle the query payload. #15

--- server/queries/posts.js
+++ server/queries/posts.js
@@ -34,29 +34,31 @@ const createSurferPost = (posts, newPost) =>
     .insert(newPost)
     .returning('*')
     .then(foundPosts => {
-      return posts.select([
-        'posts.id',
-        'posts.user_id',
-        'posts.user_rating',
-        'posts.post_content',
-        'posts.up_votes',
-        'posts.down_votes',
-        'posts.image_location_url',
-        'posts.lat',
-        'posts.lng',
-        'posts.city',
-        'posts.state',
-        'posts.zipcode',
-        'posts.created_at',
-        'posts.updated_at',
-        'users.username',
-        'users.first_name',
-        'users.last_name',
-        'users.email',
-      ])
-      .rightJoin('users', 'posts.user_id', 'users.id')
-      .where('posts.id', foundPosts[0].id)
-      .then(queriedPosts => queriedPosts[0]);
+      return posts
+        .clone()
+        .select([
+          'posts.id',
+          'posts.user_id',
+          'posts.user_rating',
+          'posts.post_content',
+          'posts.up_votes',
+          'posts.down_votes',
+          'posts.image_location_url',
+          'posts.lat',
+          'posts.lng',
+          'posts.city',
+          'posts.state',
+          'posts.zipcode',
+          'posts.created_at',
+          'posts.updated_at',
+          'users.username',
+          'users.first_name',
+          'users.last_name',
+          'users.email',
+        ])
+        .rightJoin('users', 'posts.user_id', 'users.id')
+        .where('posts.id', foundPosts[0].id)
+        .then(queriedPosts => queriedPosts[0]);
     });
 
 module.exports = {
