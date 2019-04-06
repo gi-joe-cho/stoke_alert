46298c90c48114fb9412382651bb5cef58af66c7
Joseph Cho <josephcho@Josephs-MBP-2.attlocal.net>
2019-04-05 18:06:10 -0700

    new component.jsx files for new routes

---
+++ src/containers/NewPost/NewPost.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import { Segment } from 'semantic-ui-react';
+import TabBar from '../../components/TabBar/TabBar';
+import StartMenu from '../../components/StartMenu/StartMenu';
+import NewPostComponent from '../../components/NewPost/NewPostComponent';
+class NewPost extends Component {
+	state = {
+		tabBarName: localStorage.getItem('username'),
+	}
+
+	tokenMatch = () => {
+		const { validations } = this.state;
+		let tokenStorage = localStorage.getItem('token');
+		if (validations.token != null && validations.token === tokenStorage) {
+			this.changeValidation('signedIn', true);
+		} else {
+			this.changeValidation('signedIn', false);
+		}
+	}
+	render() {
+		return (
+			<div className="wrapper-container">
+				<Segment className="div-thang" raised>
+					<TabBar
+						tabMessage={`U S E R: ${this.state.tabBarName !== null ? this.state.tabBarName : ''}`}
+						name="N e w_P o s t_"
+					/>
+					<NewPostComponent />
+					<StartMenu />
+				</Segment>
+			</div>
+		);
+	}
+}
+
+export default NewPost;
---
+++ src/containers/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import { Segment } from 'semantic-ui-react';
+import TabBar from '../../components/TabBar/TabBar';
+import StartMenu from '../../components/StartMenu/StartMenu';
+import ProfileComponent from '../../components/Profile/ProfileComponent';
+class Profile extends Component {
+	state = {
+		tabBarName: localStorage.getItem('username'),
+	}
+
+	render() {
+		return (
+			<div className="wrapper-container">
+				<Segment className="div-thang" raised>
+					<TabBar
+						tabMessage={`U S E R: ${this.state.tabBarName !== null ? this.state.tabBarName : ''}`}
+						name="P r o f i l e_"
+					/>
+					<ProfileComponent />
+					<StartMenu />
+				</Segment>
+			</div>
+		);
+	}
+}
+
+export default Profile;
