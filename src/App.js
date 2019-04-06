194bd533f0f09166765b8d61933a4449f38b4e84
Joseph Cho <josephcho@Josephs-MBP-2.attlocal.net>
2019-04-05 17:58:04 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+
 import Navbar from './containers/Navbar/Navbar';
 import Home from './containers/Home/Home';
+import NewPost from './containers/NewPost/NewPost';
+import Profile from './containers/Profile/Profile';
+import asyncComponent from './hoc/asyncComponent';
+import NoLogin from './components/NoLogin/NoLogin';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
+	state = {
+		signedIn: false,
+		token: localStorage.getItem('token')
+	}
+
+	tokenMatch () {
+		const { token } = this.state;
+		let tokenStorage = localStorage.getItem('token');
+		if (token != null && token === tokenStorage) {
+			this.setState({signedIn: true});
+		} else {
+			this.setState({signedIn: false});
+		}
+	}
+	
+	componentDidMount() {
+		this.tokenMatch();
+	}
+
 	render() {
 		return (
 			<BrowserRouter>
 				<div>
 					<Navbar />
-					<Home />
+					<Switch>
+						{/* {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null} */}
+						{/* <Redirect from="/" to="/posts" /> */}
+						{this.state.signedIn ? <Route path="/profile" component={Profile} /> : <Route path="/profile" component={NoLogin}/> }
+						{this.state.signedIn ? <Route path="/post/new" component={NewPost} /> : <Route path="/post/new" component={NoLogin} /> }
+						<Route path="/" exact component={Home} />
+						<Route render={() => <h1>Not found</h1>} />
+					</Switch>
 				</div>
 			</BrowserRouter>
 		);
