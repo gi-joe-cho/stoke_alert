21296c97900b8fde2f73f63b81c0dfb250cec21f
Joseph Cho <josephcho@Josephs-MacBook-Pro-2.local>
2019-04-08 00:24:21 -0700

    removed comments from NoLogin Component

--- src/components/NoLogin/NoLogin.jsx
+++ src/components/NoLogin/NoLogin.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 
-// import { Segment } from 'semantic-ui-react';
-// import TabBar from '../../components/TabBar/TabBar';
-// import StartMenu from '../../components/StartMenu/StartMenu';
-// import ProfileComponent from '../../components/Profile/ProfileComponent';
 class NoLogin extends Component {
 	state = {
 		tabBarName: localStorage.getItem('username'),
@@ -13,14 +9,6 @@ class NoLogin extends Component {
 		return (
 			<div className="wrapper-no-login">
 				<h1>4 0 4_L o g i n_I n_D u d e</h1>
-				{/* <Segment className="div-thang" raised>
-					<TabBar
-						tabMessage={`U S E R: ${this.state.tabBarName !== null ? this.state.tabBarName : ''}`}
-						name="N o_ M a t c h_"
-					/>
-
-					<StartMenu />
-				</Segment> */}
 			</div>
 		);
 	}
