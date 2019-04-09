1432631f517df76e6be088918a4ece46a5418f57
Joseph Cho <josephcho@Josephs-MBP-2.attlocal.net>
2019-04-08 19:40:00 -0700

    styling and aesthetics for 404 and no login pages

--- src/components/NoLogin/NoLogin.jsx
+++ src/components/NoLogin/NoLogin.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
+
+import TabBar from '../../components/TabBar/TabBar';
+import StartMenu from '../../components/StartMenu/StartMenu';
 
 class NoLogin extends Component {
 	state = {
@@ -7,8 +11,19 @@ class NoLogin extends Component {
 
 	render() {
 		return (
-			<div className="wrapper-no-login">
-				<h1>4 0 4_L o g i n_I n_D u d e</h1>
+			<div className="wrapper-container">
+				<Segment className="div-thang" raised>
+					<TabBar
+						tabMessage={`U S E R: ${this.state.tabBarName !== null ? this.state.tabBarName : ''}`}
+						name='M a p_'
+					/>
+					<Segment className="no-login-pic">
+						<Segment className="home-row" stacked>
+							<h1>4 4 0_L o g i n_I n_D u d e</h1>
+						</Segment>
+					</Segment>
+					<StartMenu />
+				</Segment>
 			</div>
 		);
 	}
