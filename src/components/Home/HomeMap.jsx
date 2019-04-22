73c70666fc764fc410dd07dc57bdc4a5e8d89aa2
Joseph Cho <josephmcho@gmail.com>
2019-04-22 16:39:26 -0700

    if else now in handler and revised naming convention

--- src/components/Home/HomeMap.jsx
+++ src/components/Home/HomeMap.jsx
@@ -1,63 +1,45 @@
-import React, { Component } from 'react';
-import { Menu, Popup, Icon, Label } from 'semantic-ui-react';
+import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const style = {
-  borderRadius: 0,
-  border: '1px solid white',
-  opacity: 0.7,
-  backgroundColor: 'grey',
-  color: 'white'
-};
-
-const AnyReactComponent = ({ opened, onClose, labelClass, mouseEnter, mouseLeave, clicked, date, upVote, downVote, city, text, color }) => (
-  <Popup
-    trigger={(
-      <Label
-        as='a'
-        className={labelClass}
-        onClick={clicked}
-        onMouseEnter={mouseEnter}
-        onMouseLeave={mouseLeave}
-        color={color}
-        pointing='below'
-        circular
-      >
-        {text}
-      </Label>
-    )}
-    style={style}
-    size='mini'
-    on='click'
-    open={opened}
-    onClose={onClose}
-  >
-    <Menu.Header>
-      {city}
-    </Menu.Header>
-    <span>Posted: {date}</span>
-    <Menu size='tiny'>
-      <Menu.Item as='a'>
-        <Icon name='thumbs up' />
-        <Label color='teal' floating>
-          {upVote}
-        </Label>
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='thumbs down' />
-        <Label color='red' floating>
-          {downVote}
-        </Label>
-      </Menu.Item>
-    </Menu>
-  </Popup>
-);
-
-let today = new Date();
-let dateToday = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+import MapMarker from './MapMarkers';
+import CurrentDate from '../../utils/currentDate';
 
 class MapHome extends Component {
-  render(){
+  markerColorHandler = (post) => {
+    let markerColor = '';
+    if (post.user_rating === 'Gnarly') {
+      markerColor = 'red';
+    } else if (post.user_rating === 'Good') {
+      markerColor = 'orange';
+    } else if (post.user_rating === 'Fair') {
+      markerColor = 'yellow';
+    } else if (post.user_rating === 'Poor') {
+      markerColor = 'teal';
+    } else if (post.user_rating === 'Flat') {
+      markerColor = 'grey';
+    }
+    return markerColor;
+  };
+  
+  markerClassHandler = (post) => {
+    let markerClass = '';
+    if (this.props.selectedPostId === post.id || this.props.clickedMarkerId === post.id || this.props.selectedMarkerId === post.id) {
+      markerClass = 'map-marker-active';
+    }
+    return markerClass;
+  };
+  
+  popUpHandler = (post) => {
+    let popOpen = false;
+    if (this.props.clickedPostId === post.id || this.props.clickedMarkerId === post.id) {
+      popOpen = true;
+    } else {
+      popOpen = false;
+    }
+    return popOpen;
+  };
+  
+  render () {
     return(
       <div style={{ height: '100%', width: '100%' }}>
         {
@@ -69,10 +51,10 @@ class MapHome extends Component {
                 defaultZoom={this.props.zoom}
                 onChange={this.props.changeBounds}
               >
-                <AnyReactComponent
+                <MapMarker
                   lat={this.props.userLocation.lat}
                   lng={this.props.userLocation.lng}
-                  date={dateToday}
+                  date={<CurrentDate />}
                   text="Current Location"
                   city='Current Location'
                 />
@@ -80,41 +62,19 @@ class MapHome extends Component {
                   this.props.posts !== undefined
                     ? (
                       this.props.posts.map(post => {
-                        let markerColor = '';
-                        let markerClass = '';
-                        let popOpen = false;
-                        if (post.user_rating === 'Gnarly') {
-                          markerColor = 'red';
-                        } else if (post.user_rating === 'Good') {
-                          markerColor = 'orange';
-                        } else if (post.user_rating === 'Fair') {
-                          markerColor = 'yellow';
-                        } else if (post.user_rating === 'Poor') {
-                          markerColor = 'teal';
-                        } else if (post.user_rating === 'Flat') {
-                          markerColor = 'grey';
-                        }
-                        if (this.props.selectedPostId === post.id || this.props.clickedPostId === post.id) {
-                          markerClass = 'map-marker-active';
-                        }
-                        if (this.props.clickedPostId === post.id || this.props.clickedMarkerId === post.id) {
-                          popOpen = true;
-                        } else {
-                          popOpen = false;
-                        }
                         return (
-                          <AnyReactComponent
-                            labelClass={markerClass}
+                          <MapMarker
+                            labelClass={this.markerClassHandler(post)}
                             key={post.id}
                             lat={post.lat}
                             lng={post.lng}
                             text={post.user_rating}
                             city={post.city}
-                            color={markerColor}
+                            color={this.markerColorHandler(post)}
                             upVote={post.up_votes}
                             downVote={post.down_votes}
                             date={post.created_at.substring(0, 10)}
-                            opened={popOpen}
+                            opened={this.popUpHandler(post)}
                             onClose={() => {
                               this.props.onCloseHandler();
                             }}
@@ -131,7 +91,7 @@ class MapHome extends Component {
                 }
               </GoogleMapReact>
             )
-            : null
+            : window.location.reload
         }
       </div>
     );
