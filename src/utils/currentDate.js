203423be159b10d46c0225a06bda3c895c74f987
Joseph Cho <josephmcho@gmail.com>
2019-04-23 10:47:05 -0700

    camel case to currentDate.js

--- src/utils/currentDate.js
+++ src/utils/currentDate.js
@@ -1,7 +1,7 @@
-const CurrentDate = () => {
+const currentDate = () => {
   let currentDate = new Date();
   let formattedCurrentDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
   return formattedCurrentDate;
 }
 
-export default CurrentDate;
+export default currentDate;
