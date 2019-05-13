a41602665aacf0f6f4f7880aff90e301f2d45aea
Joseph Cho <josephmcho@gmail.com>
2019-05-13 12:04:32 -0700

    refactor and delete console.logs

--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -39,7 +39,7 @@ class Home extends Component {
         });
       },
       () => {
-        this.setState({ loading: false });
+        this.setState({ loading: true });
       }
     );
   }
@@ -95,6 +95,7 @@ class Home extends Component {
               popOpen={popOpen}
               zoom={zoom}
             />
+            
           </Segment>
           <Segment className="map-list-container" placeholder raised>
             <Segment className='map-list-header'>
