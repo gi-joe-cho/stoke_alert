897f52d4f79f8d0ada430a97ac73f8b2607c82f6
Joseph Cho <gi.joecho@yahoo.com>
2019-05-23 16:37:53 -0700

    added check handler to see if url is retrievable and place a 404 page if not found. #23

--- src/containers/Post/PostDetail.jsx
+++ src/containers/Post/PostDetail.jsx
@@ -3,23 +3,45 @@ import { Segment } from 'semantic-ui-react';
 import TabBar from '../../components/TabBar/TabBar';
 import StartMenu from '../../components/StartMenu/StartMenu';
 import PostDetailComponent from '../../components/Post/PostDetailComponent';
+import NoMatch from '../../components/NoMatch/NoMatch';
 
 class PostDetail extends Component {
   state = {
     tabBarName: localStorage.getItem('username'),
+    loading: false
+  }
+  
+  checkPost = async () => {
+    const response = await fetch(`${process.env.REACT_APP_DEV_API_DOMAIN}/posts/${this.props.match.params.post_id}`);
+    if (response.status !== 404) {
+      this.setState({ loading: true });
+    }
+    else if (response.status === 404) {
+      console.log('page not found');
+      this.setState({ loading: false });
+    }
   }
 
   render() {
+    this.checkPost();
     return (
-      <div className="wrapper-container">
-        <Segment className="div-thang" raised>
-          <TabBar
-            tabMessage={`U S E R: ${this.state.tabBarName !== null ? this.state.tabBarName : ''}`}
-            name="P o s t_D e t a i l_"
-          />
-          <PostDetailComponent />
-          <StartMenu />
-        </Segment>
+      <div>
+        {
+          this.state.loading
+            ? (
+              <div className="wrapper-container">
+                <Segment className="div-thang" raised>
+                  <TabBar
+                    tabMessage={`U S E R: ${this.state.tabBarName !== null ? this.state.tabBarName : ''}`}
+                    name="P o s t_D e t a i l_"
+                  />
+                  <PostDetailComponent />
+                  <StartMenu />
+                </Segment>
+              </div>
+            )
+            : <NoMatch /> 
+        }
       </div>
     );
   }
